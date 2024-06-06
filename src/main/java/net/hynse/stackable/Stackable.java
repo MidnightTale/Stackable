49fee92239f56e7b0bbd4172eb73685ed5d05033
MidnightTale <midnighttale24@gmail.com>
2024-06-06 13:14:48 +0700

    Initial commit

---
+++ src/main/java/net/hynse/stackable/Stackable.java
@@ -0,0 +1,17 @@
+package net.hynse.stackable;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Stackable extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
