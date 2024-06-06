4a5cbe89acb61012b50b69d511faeccf001bfec2
MidnightTale <midnighttale24@gmail.com>
2024-06-06 14:00:44 +0700

    optimize v2

--- src/main/java/net/hynse/stackable/Stackable.java
+++ src/main/java/net/hynse/stackable/Stackable.java
@@ -4,9 +4,9 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Stackable extends JavaPlugin implements Listener {
@@ -16,163 +16,45 @@ public class Stackable extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    private void setMaxStackSize(ItemStack itemStack, int maxStackSize) {
-        if (itemStack != null) {
-            Material material = itemStack.getType();
-            if (isStackableItem(material)) {
-                itemStack.setAmount(Math.min(itemStack.getAmount(), maxStackSize));
-            }
+    @EventHandler
+    public void onPrepareItemCraft(PrepareItemCraftEvent event) {
+        ItemStack result = event.getInventory().getResult();
+        assert result != null;
+        ItemMeta meta = result.getItemMeta();
+        if (isStackableItem(result.getType()) && meta != null) {
+            meta.setMaxStackSize(getMaxStackSize(result.getType()));;
+            result.setItemMeta(meta);
         }
     }
 
     private boolean isStackableItem(Material material) {
-        switch (material) {
-            case POTION:
-            case MINECART:
-            case CHEST_MINECART:
-            case FURNACE_MINECART:
-            case TNT_MINECART:
-            case HOPPER_MINECART:
-            case OAK_BOAT:
-            case SPRUCE_BOAT:
-            case BIRCH_BOAT:
-            case JUNGLE_BOAT:
-            case ACACIA_BOAT:
-            case DARK_OAK_BOAT:
-            case MANGROVE_BOAT:
-            case CHERRY_BOAT:
-            case OAK_CHEST_BOAT:
-            case SPRUCE_CHEST_BOAT:
-            case BIRCH_CHEST_BOAT:
-            case JUNGLE_CHEST_BOAT:
-            case ACACIA_CHEST_BOAT:
-            case DARK_OAK_CHEST_BOAT:
-            case MANGROVE_CHEST_BOAT:
-            case CHERRY_CHEST_BOAT:
-            case TOTEM_OF_UNDYING:
-            case WHITE_SHULKER_BOX:
-            case ORANGE_SHULKER_BOX:
-            case MAGENTA_SHULKER_BOX:
-            case LIGHT_BLUE_SHULKER_BOX:
-            case YELLOW_SHULKER_BOX:
-            case LIME_SHULKER_BOX:
-            case PINK_SHULKER_BOX:
-            case GRAY_SHULKER_BOX:
-            case LIGHT_GRAY_SHULKER_BOX:
-            case CYAN_SHULKER_BOX:
-            case PURPLE_SHULKER_BOX:
-            case BLUE_SHULKER_BOX:
-            case BROWN_SHULKER_BOX:
-            case GREEN_SHULKER_BOX:
-            case RED_SHULKER_BOX:
-            case BLACK_SHULKER_BOX:
-            case WHITE_BED:
-            case ORANGE_BED:
-            case MAGENTA_BED:
-            case LIGHT_BLUE_BED:
-            case YELLOW_BED:
-            case LIME_BED:
-            case PINK_BED:
-            case GRAY_BED:
-            case LIGHT_GRAY_BED:
-            case CYAN_BED:
-            case PURPLE_BED:
-            case BLUE_BED:
-            case BROWN_BED:
-            case GREEN_BED:
-            case RED_BED:
-            case BLACK_BED:
-                return true;
-            default:
-                return false;
-        }
+        return switch (material) {
+            case POTION, MINECART, CHEST_MINECART, FURNACE_MINECART, TNT_MINECART, HOPPER_MINECART, OAK_BOAT,
+                 SPRUCE_BOAT, BIRCH_BOAT, JUNGLE_BOAT, ACACIA_BOAT, DARK_OAK_BOAT, MANGROVE_BOAT, CHERRY_BOAT,
+                 OAK_CHEST_BOAT, SPRUCE_CHEST_BOAT, BIRCH_CHEST_BOAT, JUNGLE_CHEST_BOAT, ACACIA_CHEST_BOAT,
+                 DARK_OAK_CHEST_BOAT, MANGROVE_CHEST_BOAT, CHERRY_CHEST_BOAT, TOTEM_OF_UNDYING, WHITE_SHULKER_BOX,
+                 ORANGE_SHULKER_BOX, MAGENTA_SHULKER_BOX, LIGHT_BLUE_SHULKER_BOX, YELLOW_SHULKER_BOX, LIME_SHULKER_BOX,
+                 PINK_SHULKER_BOX, GRAY_SHULKER_BOX, LIGHT_GRAY_SHULKER_BOX, CYAN_SHULKER_BOX, PURPLE_SHULKER_BOX,
+                 BLUE_SHULKER_BOX, BROWN_SHULKER_BOX, GREEN_SHULKER_BOX, RED_SHULKER_BOX, BLACK_SHULKER_BOX, WHITE_BED,
+                 ORANGE_BED, MAGENTA_BED, LIGHT_BLUE_BED, YELLOW_BED, LIME_BED, PINK_BED, GRAY_BED, LIGHT_GRAY_BED,
+                 CYAN_BED, PURPLE_BED, BLUE_BED, BROWN_BED, GREEN_BED, RED_BED, BLACK_BED -> true;
+            default -> false;
+        };
     }
 
     private int getMaxStackSize(Material material) {
-        switch (material) {
-            case POTION:
-            case LINGERING_POTION:
-            case SPLASH_POTION:
-            case MINECART:
-            case CHEST_MINECART:
-            case FURNACE_MINECART:
-            case TNT_MINECART:
-            case HOPPER_MINECART:
-            case OAK_BOAT:
-            case SPRUCE_BOAT:
-            case BIRCH_BOAT:
-            case JUNGLE_BOAT:
-            case ACACIA_BOAT:
-            case DARK_OAK_BOAT:
-            case MANGROVE_BOAT:
-            case CHERRY_BOAT:
-            case OAK_CHEST_BOAT:
-            case SPRUCE_CHEST_BOAT:
-            case BIRCH_CHEST_BOAT:
-            case JUNGLE_CHEST_BOAT:
-            case ACACIA_CHEST_BOAT:
-            case DARK_OAK_CHEST_BOAT:
-            case MANGROVE_CHEST_BOAT:
-            case CHERRY_CHEST_BOAT:
-            case WHITE_SHULKER_BOX:
-            case ORANGE_SHULKER_BOX:
-            case MAGENTA_SHULKER_BOX:
-            case LIGHT_BLUE_SHULKER_BOX:
-            case YELLOW_SHULKER_BOX:
-            case LIME_SHULKER_BOX:
-            case PINK_SHULKER_BOX:
-            case GRAY_SHULKER_BOX:
-            case LIGHT_GRAY_SHULKER_BOX:
-            case CYAN_SHULKER_BOX:
-            case PURPLE_SHULKER_BOX:
-            case BLUE_SHULKER_BOX:
-            case BROWN_SHULKER_BOX:
-            case GREEN_SHULKER_BOX:
-            case RED_SHULKER_BOX:
-            case BLACK_SHULKER_BOX:
-                return 16;
-            case TOTEM_OF_UNDYING:
-                return 4;
-            case WHITE_BED:
-            case ORANGE_BED:
-            case MAGENTA_BED:
-            case LIGHT_BLUE_BED:
-            case YELLOW_BED:
-            case LIME_BED:
-            case PINK_BED:
-            case GRAY_BED:
-            case LIGHT_GRAY_BED:
-            case CYAN_BED:
-            case PURPLE_BED:
-            case BLUE_BED:
-            case BROWN_BED:
-            case GREEN_BED:
-            case RED_BED:
-            case BLACK_BED:
-                return 8;
-            default:
-                return material.getMaxStackSize();
-        }
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        updateInventory(event.getPlayer().getInventory());
-    }
-
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        ItemStack currentItem = event.getCurrentItem();
-        if (currentItem != null && isStackableItem(currentItem.getType())) {
-            setMaxStackSize(currentItem, getMaxStackSize(currentItem.getType()));
-        }
-    }
-
-    private void updateInventory(Inventory inventory) {
-        for (ItemStack itemStack : inventory.getContents()) {
-            if (itemStack != null && isStackableItem(itemStack.getType())) {
-                setMaxStackSize(itemStack, getMaxStackSize(itemStack.getType()));
-            }
-        }
+        return switch (material) {
+            case POTION, MINECART, CHEST_MINECART, FURNACE_MINECART, TNT_MINECART, HOPPER_MINECART, OAK_BOAT,
+                 SPRUCE_BOAT, BIRCH_BOAT, JUNGLE_BOAT, ACACIA_BOAT, DARK_OAK_BOAT, MANGROVE_BOAT, CHERRY_BOAT,
+                 OAK_CHEST_BOAT, SPRUCE_CHEST_BOAT, BIRCH_CHEST_BOAT, JUNGLE_CHEST_BOAT, ACACIA_CHEST_BOAT,
+                 DARK_OAK_CHEST_BOAT, MANGROVE_CHEST_BOAT, CHERRY_CHEST_BOAT, WHITE_SHULKER_BOX, ORANGE_SHULKER_BOX,
+                 MAGENTA_SHULKER_BOX, LIGHT_BLUE_SHULKER_BOX, YELLOW_SHULKER_BOX, LIME_SHULKER_BOX, PINK_SHULKER_BOX,
+                 GRAY_SHULKER_BOX, LIGHT_GRAY_SHULKER_BOX, CYAN_SHULKER_BOX, PURPLE_SHULKER_BOX, BLUE_SHULKER_BOX,
+                 BROWN_SHULKER_BOX, GREEN_SHULKER_BOX, RED_SHULKER_BOX, BLACK_SHULKER_BOX -> 16;
+            case TOTEM_OF_UNDYING -> 4;
+            case WHITE_BED, ORANGE_BED, MAGENTA_BED, LIGHT_BLUE_BED, YELLOW_BED, LIME_BED, PINK_BED, GRAY_BED,
+                 LIGHT_GRAY_BED, CYAN_BED, PURPLE_BED, BLUE_BED, BROWN_BED, GREEN_BED, RED_BED, BLACK_BED -> 8;
+            default -> material.getMaxStackSize();
+        };
     }
 }
